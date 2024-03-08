0a114bbc4ffe05117056c9bb9d491aa012111b61
ErikWebDeveloper <erikcodedeveloper@gmail.com>
2024-03-08 21:25:19 +0100

    tranlate errors

--- models/subscripcioModel.php
+++ models/subscripcioModel.php
@@ -18,10 +18,8 @@ class SubscripcioModel {
             $this->conexion = new MongoDB\Driver\Manager("mongodb://{$this->host}:{$this->puerto}");
             // Conexión exitosa a MongoDB (no se retorna mensaje de éxito en producción)
         } catch (MongoDB\Driver\Exception\Exception $e) {
-            //$this->enviarRespuestaJSON(['error' => true, 'mensaje' => "Error de conexión: " . $e->getMessage()]);
-            //exit;
-            $response = ['error' => true, 'mensaje' => "Error de conexión: " . $e->getMessage()];
-            return;
+            //return $response = ['error' => true, 'mensaje' => "Error de conexión: " . $e->getMessage()];
+            return $response = ['error' => true, 'mensaje' => "Error de connexió a la base de dades."];
         }
     }
 
@@ -33,15 +31,19 @@ class SubscripcioModel {
             $resultado = $this->conexion->executeBulkWrite("{$this->baseDatos}.{$this->coleccion}", $bulk);
 
             if ($resultado->getInsertedCount() > 0) {
-                //$this->enviarRespuestaJSON(['error' => false, 'mensaje' => "Documento insertado correctamente.", 'id' => (string) $id]);
-                return ['error' => false, 'mensaje' => "Documento insertado correctamente.", 'id' => (string) $id];
+                //return ['error' => false, 'mensaje' => "Documento insertado correctamente.", 'id' => (string) $id];
+                return ['error' => false, 'mensaje' => "La seva sol·licitud s'ha processat correctament."];
+
             } else {
-                //$this->enviarRespuestaJSON(['error' => true, 'mensaje' => "Error al insertar el documento."]);
-                return ['error' => true, 'mensaje' => "Error al insertar el documento."];
+                //return ['error' => true, 'mensaje' => "Error al insertar el documento."];
+                return ['error' => true, 'mensaje' => "La seva sol·licitud no s'ha processat correctament."];
+
             }
         } catch (MongoDB\Driver\Exception\Exception $e) {
             //$this->enviarRespuestaJSON(['error' => true, 'mensaje' => "Error al insertar: " . $e->getMessage()]);
-            return ['error' => true, 'mensaje' => "Error al insertar: " . $e->getMessage()];
+            //return ['error' => true, 'mensaje' => "Error al insertar: " . $e->getMessage()];
+            return ['error' => true, 'mensaje' => "La seva sol·licitud no s'ha processat."];
+
         }
     }
 
