48b5c3efe2fca4b206bcec0476b54c4d1717cf6e
ErikWebDeveloper <erikcodedeveloper@gmail.com>
2024-03-21 14:54:05 +0100

    test

--- libs/storeImage.php
+++ libs/storeImage.php
@@ -24,7 +24,7 @@ function storeImage($dataJSON) {
         } else {
             return ['error' => true, 'mensaje' => "Ha ocurrido un error al almacenar la imagen."];
         }*/
-        return ['error' => true, 'mensaje' => "La imagen se ha almacenado correctamente."];
+        return ['error' => true, 'mensaje' => $directorioImagenes];
 
     } catch ( Exception $e){
         return ['error' => true, 'mensaje' => $e->getMessage()];
