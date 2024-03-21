f4abed308e62152715ee3bd12cd14ad8dd1db3c5
ErikWebDeveloper <erikcodedeveloper@gmail.com>
2024-03-21 14:35:20 +0100

    test

--- libs/storeImage.php
+++ libs/storeImage.php
@@ -3,21 +3,21 @@
 function storeImage($dataJSON) {
     try{
         // Ruta donde se guardarán las imágenes en el servidor
-        $directorioImagenes = $_SERVER['DOCUMENT_ROOT'] . '/AppData/img';
+        $directorioImagenes = '../AppData/img';
         
-        /*
         // Obtener la información de la imagen del objeto JSON
         $nombreImagen = $dataJSON[0]['usuarios']['user_foto']['nombre'];
         $tipoImagen = $dataJSON[0]['usuarios']['user_foto']['tipo'];
         $contenidoImagen = $dataJSON[0]['usuarios']['user_foto']['contenido'];
-    
+        
         // Decodificar el contenido de la imagen
         $contenidoDecodificado = base64_decode(substr($contenidoImagen, strpos($contenidoImagen, ',') + 1));
-    
+        
+        /*
         // Guardar la imagen en el servidor
         $rutaImagen = $directorioImagenes . $nombreImagen;
         $resultado = file_put_contents($rutaImagen, $contenidoDecodificado);
-    
+        
         // Verificar si la imagen se guardó correctamente
         if ($resultado !== false) {
             return ['error' => false, 'mensaje' => "La imagen se ha almacenado correctamente."];
@@ -27,6 +27,6 @@ function storeImage($dataJSON) {
         return ['error' => true, 'mensaje' => "La imagen se ha almacenado correctamente."];
 
     } catch ( Exception $e){
-        return ['error' => true, 'mensaje' => "Ha ocurrido un error al almacenar la imagen."];
+        return ['error' => true, 'mensaje' => "Ha ocurrido un error al almacenar la imagen." . $e->getMessage()];
     }
 }
