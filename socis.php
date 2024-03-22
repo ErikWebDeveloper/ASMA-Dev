744050f2fd7ebb1783a0212011f3fa5d579253e1
ErikWebDeveloper <erikcodedeveloper@gmail.com>
2024-03-22 14:11:32 +0100

    update

--- socis.php
+++ socis.php
@@ -30,6 +30,70 @@ if (!isset($_SESSION['csrf_token'])) {
     <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
 
     <link rel="stylesheet" href="/assets/css/main.css">
+
+    <style>
+        .container {
+    width: 100%;
+    max-width: 500px;
+    margin: 5px;
+}
+ 
+.container h1 {
+    color: #ffffff;
+}
+ 
+.section {
+    background-color: #ffffff;
+    padding: 50px 30px;
+    border: 1.5px solid #b2b2b2;
+    border-radius: 0.25em;
+    box-shadow: 0 20px 25px rgba(0, 0, 0, 0.25);
+}
+ 
+#my-qr-reader {
+    padding: 20px !important;
+    border: 1.5px solid #b2b2b2 !important;
+    border-radius: 8px;
+}
+ 
+#my-qr-reader img[alt="Info icon"] {
+    display: none;
+}
+ 
+#my-qr-reader img[alt="Camera based scan"] {
+    width: 100px !important;
+    height: 100px !important;
+}
+ 
+button {
+    padding: 10px 20px;
+    border: 1px solid #b2b2b2;
+    outline: none;
+    border-radius: 0.25em;
+    color: white;
+    font-size: 15px;
+    cursor: pointer;
+    margin-top: 15px;
+    margin-bottom: 10px;
+    background-color: #008000ad;
+    transition: 0.3s background-color;
+}
+ 
+button:hover {
+    background-color: #008000;
+}
+ 
+#html5-qrcode-anchor-scan-type-change {
+    text-decoration: none !important;
+    color: #1d9bf0;
+}
+ 
+video {
+    width: 100% !important;
+    border: 1px solid #b2b2b2 !important;
+    border-radius: 0.25em;
+}
+    </style>
 </head>
 
 <body>
@@ -39,47 +103,45 @@ if (!isset($_SESSION['csrf_token'])) {
     <!-- NavBar -->
     <?php require_once './components/navbar.php'; ?>
 
-    <div>
-        <div id="scanner-container">
-            <video id="scanner-video" playsinline style="display: none;"></video>
-            <canvas id="scanner-canvas" style="display: none;"></canvas>
+    <div class="container">
+        <h1>Scan QR Codes</h1>
+        <div class="section">
+            <div id="my-qr-reader">
+            </div>
         </div>
-        <div id="resultado-escaneo"></div>
-        <button id="start-scan-button">Empezar a escanear</button>
     </div>
+    <script
+        src="https://unpkg.com/html5-qrcode">
+    </script>
+    <script>
+        function domReady(fn) {
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+ 
+domReady(function () {
+ 
+    // If found you qr code
+    function onScanSuccess(decodeText, decodeResult) {
+        alert("You Qr is : " + decodeText, decodeResult);
+    }
+ 
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});
+    </script>
 
     <!-- Footer -->
     <?php require_once './components/footer.php' ?>
 
-    <script>
-        // Inicializar el escáner
-        let scanner = null;
-
-        // Función para comenzar el escaneo
-        function startScan() {
-          scanner = new Instascan.Scanner({ video: document.getElementById('scanner-video') });
-
-          // Escuchar el evento de escaneo
-          scanner.addListener('scan', function(content) {
-            // Mostrar el resultado del escaneo
-            document.getElementById('resultado-escaneo').innerText = content;
-          });
-
-          // Obtener las cámaras disponibles
-          Instascan.Camera.getCameras().then(function(cameras) {
-            if (cameras.length > 0) {
-              // Iniciar el escaneo con la primera cámara disponible
-              scanner.start(cameras[0]);
-            } else {
-              console.error('No se detectó ninguna cámara.');
-            }
-          }).catch(function(e) {
-            console.error(e);
-          });
-        }
-
-        // Obtener el botón "Empezar a escanear" y agregar un evento de clic
-        document.getElementById('start-scan-button').addEventListener('click', startScan);
-    </script>
 <body>
 </html>
