34855c947b6165242a0659ea824ac43b8df75954
ErikWebDeveloper <erikcodedeveloper@gmail.com>
2024-03-24 21:28:06 +0100

    remove compresion

--- models/subscripcioImageModel.php
+++ models/subscripcioImageModel.php
@@ -60,10 +60,10 @@ class SubscripcioImageModel{
             $contenidoImagen = $imageData['contenido'];
                 
             // Decodificar el contenido de la imagen
-            //$contenidoDecodificado = base64_decode(substr($contenidoImagen, strpos($contenidoImagen, ',') + 1));
+            $contenidoDecodificado = base64_decode(substr($contenidoImagen, strpos($contenidoImagen, ',') + 1));
                 
-            // Decodificar y comprimir imagen
-            $contenidoDecodificado = $this->procesarImagen($contenidoImagen);
+            // Decodificar y comprimir imagen ***** Hay que areglar
+            //$contenidoDecodificado = $this->procesarImagen($contenidoImagen);
 
             // Guardar la imagen en el servidor
             $rutaImagen = $directorioImagenes . $nombreImagen;
