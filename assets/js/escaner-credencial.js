a1162eb8023b96bb72454a373bc94d48791353ff
ErikWebDeveloper <erikcodedeveloper@gmail.com>
2024-03-25 21:07:39 +0100

    test

--- assets/js/escaner-credencial.js
+++ assets/js/escaner-credencial.js
@@ -13,12 +13,18 @@ domReady(function () {
   // If found you qr code
   function onScanSuccess(decodeText, decodeResult) {
     alert("You Qr is : " + decodeText);
-    alert(decodeResult)
+    alert(decodeResult);
+  }
+  // If failure you qr code
+  function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+    console.warn(`Code scan error = ${error}`);
   }
 
   let htmlscanner = new Html5QrcodeScanner("my-qr-reader", {
     fps: 10,
-    qrbos: 250,
+    qrbox: { width: 250, height: 250 },
   });
   htmlscanner.render(onScanSuccess);
 });
