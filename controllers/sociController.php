f6c102f9a9a2c4a6641c36d09a0c1fa0af022149
ErikWebDeveloper <erikcodedeveloper@gmail.com>
2024-03-26 20:08:53 +0100

    fix

--- controllers/sociController.php
+++ controllers/sociController.php
@@ -59,7 +59,7 @@ class Soci{
 
     private function isValidData(){
         // Validar Correo Existente
-        $query = $this->model->find(["_id" => $this->request['id']);
+        $query = $this->model->find(["_id" => $this->request['id']]);
         if($query == null){
             $this->response = [ "error" => true, "mensaje" => 'Sembla que aquest soci no esta en la nostra base de dades.' . $this->request['id']];
             $this->sendResponse(200, $this->response);
