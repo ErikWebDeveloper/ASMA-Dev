bc9fece6eca5fcffa8ca7d54712adf189109f0b8
ErikWebDeveloper <erikcodedeveloper@gmail.com>
2024-04-12 17:52:38 +0200

    update

--- assets/js/escaner-credencial.js
+++ assets/js/escaner-credencial.js
@@ -48,14 +48,20 @@ domReady(function () {
           ? "Credencial invalida"
           : "Credencial correcta!";
         document.getElementById("log-message").innerHTML = `
-        <div class="toast align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
-          <div class="d-flex">
+        <div class="toast-container position-fixed bottom-0 end-0 p-3">
+          <div id="liveToast" class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+              <img src="..." class="rounded me-2" alt="...">
+              <strong class="me-auto">Bootstrap</strong>
+              <small>11 mins ago</small>
+              <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
             <div class="toast-body">
               ${message}
             </div>
-            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
           </div>
         </div>
+
         `;
       })
       .catch((error) => {
